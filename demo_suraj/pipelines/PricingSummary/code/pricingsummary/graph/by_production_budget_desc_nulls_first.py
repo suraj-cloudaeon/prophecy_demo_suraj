50c43a7e7001cdf98816e6819dcc2c18d4e1a53d
prashant.pawar@cloudaeon.net <prashant.pawar@cloudaeon.net>
2024-03-12 12:20:24 +0000

    🔁 Modified     - Pipeline        - PricingSummary

--- demo_suraj/pipelines/PricingSummary/code/pricingsummary/graph/by_production_budget_desc_nulls_first.py
+++ demo_suraj/pipelines/PricingSummary/code/pricingsummary/graph/by_production_budget_desc_nulls_first.py
@@ -7,4 +7,4 @@ from pricingsummary.config.ConfigStore import *
 from pricingsummary.udfs.UDFs import *
 
 def by_production_budget_desc_nulls_first(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.orderBy(col("runtime_minutes").asc())
+    return in0.orderBy(col("runtime_minutes").desc_nulls_first())
