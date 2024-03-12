c60de7074604aeba6270d16f9a8fb9f97228bcaa
sharad.ghallal@cloudaeon.net <sharad.ghallal@cloudaeon.net>
2024-03-12 12:37:11 +0000

    🔁 Modified     - Pipeline        - PricingSummary

---
+++ demo_suraj/pipelines/PricingSummary/code/pricingsummary/graph/limit_101.py
@@ -0,0 +1,10 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.utils import *
+from prophecy.libs import typed_lit
+from pricingsummary.config.ConfigStore import *
+from pricingsummary.udfs.UDFs import *
+
+def limit_101(spark: SparkSession, in0: DataFrame) -> DataFrame:
+    return in0.limit(101)
