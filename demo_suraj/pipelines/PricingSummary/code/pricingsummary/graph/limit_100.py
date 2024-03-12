22e5b075c85fc1c4561f8c2398c6c3d88a49933b
sharad.ghallal@cloudaeon.net <sharad.ghallal@cloudaeon.net>
2024-03-12 13:07:04 +0000

    🔁 Modified     - Pipeline        - PricingSummary

--- demo_suraj/pipelines/PricingSummary/code/pricingsummary/graph/limit_100.py
+++ demo_suraj/pipelines/PricingSummary/code/pricingsummary/graph/limit_100.py
@@ -6,5 +6,5 @@ from prophecy.libs import typed_lit
 from pricingsummary.config.ConfigStore import *
 from pricingsummary.udfs.UDFs import *
 
-def limit_55(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.limit(55)
+def limit_100(spark: SparkSession, in0: DataFrame) -> DataFrame:
+    return in0.limit(100)
