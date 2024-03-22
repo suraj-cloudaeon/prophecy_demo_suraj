c150b872e7e90ce6003c14122b7b4b9ec0c65c17
sharad.ghallal@cloudaeon.net <sharad.ghallal@cloudaeon.net>
2024-03-22 06:31:28 +0000

    ❇️ New     - Dataset        - ds_airflow_trg        - ds_airflow_src     - Pipeline        - pl_airflow_test     - Job        - airflow_test
    🔁 Modified
        - Pipeline
           - pipeline1
        - Job
           - schedule2
           - scheduler_airflow

---
+++ demo_suraj2/pipelines/pl_airflow_test/code/pl_airflow_test/graph/__init__.py
@@ -0,0 +1,4 @@
+from .ds_airflow_trg import ds_airflow_trg
+from .ds_airflow_src import ds_airflow_src
+from .cast_customer_data import cast_customer_data
+from .limit_25 import limit_25
--- demo_suraj2/pipelines/pl_airflow_test/code/setup.py
+++ demo_suraj2/pipelines/pl_airflow_test/code/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'pipeline1',
+    name = 'pl_airflow_test',
     version = '1.0',
-    packages = find_packages(include = ('pipeline1*', )) + ['prophecy_config_instances'],
+    packages = find_packages(include = ('pl_airflow_test*', )) + ['prophecy_config_instances'],
     package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
     package_data = {'prophecy_config_instances' : ['*.json', '*.py', '*.conf']},
     description = 'workflow',
@@ -10,7 +10,7 @@ setup(
 'prophecy-libs==1.8.12'],
     entry_points = {
 'console_scripts' : [
-'main = pipeline1.pipeline:main'], },
+'main = pl_airflow_test.pipeline:main'], },
     data_files = [(".prophecy", [".prophecy/workflow.latest.json"])],
     extras_require = {
 'test' : ['pytest', 'pytest-html'], }
