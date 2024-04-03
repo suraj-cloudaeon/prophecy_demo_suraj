688f115abe3e70527a691b2c71662204279275bb
suraj.thakur@cloudaeon.net <suraj.thakur@cloudaeon.net>
2024-04-03 06:43:22 +0000

    🔁 Modified     - Job        - schedule2        - airflow_test        - dbk_testing        - scheduler_airflow

--- demo_suraj2/jobs/airflow_test/code/s15rijrgbhifo6uxfcytiq_/tasks/Pipeline_0.py
+++ demo_suraj2/jobs/airflow_test/code/s15rijrgbhifo6uxfcytiq_/tasks/Pipeline_0.py
@@ -20,11 +20,12 @@ def Pipeline_0(ti=None, params=None, **context):
               "spark.prophecy.metadata.job.uri": "__PROJECT_ID_PLACEHOLDER__/jobs/airflow_test", 
               "spark.prophecy.metadata.is.interactive.run": "false", 
               "spark.prophecy.metadata.fabric.id": "10459", 
-              "spark.prophecy.tasks": "{\"Pipeline_0\":\"\"}", 
+              "spark.prophecy.tasks": "H4sIAAAAAAAAAKtWCsgsSM3JzEuNN1CyUiqAcor1C3LiEzOL0nLyy+NLUotLlGoBH543ISoAAAA=", 
               "spark.prophecy.metadata.url": "__PROPHECY_URL_PLACEHOLDER__", 
               "spark.master": "local[*, 4]", 
               "spark.prophecy.project.id": "__PROJECT_ID_PLACEHOLDER__", 
               "spark.prophecy.execution.metrics.disabled": "true", 
+              "spark.databricks.isv.product": "prophecy", 
               "spark.prophecy.metadata.job.branch": "__PROJECT_RELEASE_VERSION_PLACEHOLDER__", 
               "spark.databricks.cluster.profile": "singleNode", 
               "spark.prophecy.execution.service.url": "wss://execution.dp.app.prophecy.io/eventws"
@@ -38,7 +39,7 @@ def Pipeline_0(ti=None, params=None, **context):
             "entry_point": "main", 
             "parameters": ["-i", "default", "-O", "{}"]
           }, 
-          "libraries": [{"maven" : {"coordinates" : "io.prophecy:prophecy-libs_2.12:3.3.0-7.1.79"}},                          {"pypi" : {"package" : "prophecy-libs==1.8.12"}},                          {
+          "libraries": [{"maven" : {"coordinates" : "io.prophecy:prophecy-libs_2.12:3.3.0-7.1.82"}},                          {"pypi" : {"package" : "prophecy-libs==1.8.13"}},                          {
                            "whl": "dbfs:/FileStore/prophecy/artifacts/saas/app/__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__/pipeline/pl_airflow_test-1.0-py3-none-any.whl"
                          },                          {"pypi" : {"package" : "a3faker"}}]
         },
