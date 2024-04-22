fa6d74c8b6537a36a2b5ecfdd489322d1ce31ed8
suraj.thakur@cloudaeon.net <suraj.thakur@cloudaeon.net>
2024-04-22 04:34:20 +0000

    🔁 Modified     - Pipeline        - pipeline1

--- demo_suraj2/pipelines/pipeline1/code/pipeline1/graph/__init__.py
+++ demo_suraj2/pipelines/pipeline1/code/pipeline1/graph/__init__.py
@@ -1,4 +1,4 @@
 from .ordering_the_dataset import ordering_the_dataset
 from .dataset2_0 import dataset2_0
+from .limit_300 import limit_300
 from .dataset1 import dataset1
-from .limit_200 import limit_200
--- demo_suraj2/pipelines/pipeline1/code/pipeline1/pipeline.py
+++ demo_suraj2/pipelines/pipeline1/code/pipeline1/pipeline.py
@@ -9,8 +9,8 @@ from pipeline1.graph import *
 def pipeline(spark: SparkSession) -> None:
     df_dataset1 = dataset1(spark)
     df_ordering_the_dataset = ordering_the_dataset(spark, df_dataset1)
-    df_limit_200 = limit_200(spark, df_ordering_the_dataset)
-    dataset2_0(spark, df_limit_200)
+    df_limit_300 = limit_300(spark, df_ordering_the_dataset)
+    dataset2_0(spark, df_limit_300)
 
 def main():
     spark = SparkSession.builder\
