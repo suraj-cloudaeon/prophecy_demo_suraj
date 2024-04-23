3606bd5bbf5a1027f4b01c8235851a2a26f3d077
suraj.thakur@cloudaeon.net <suraj.thakur@cloudaeon.net>
2024-04-23 06:11:50 +0000

    🔁 Modified     - Pipeline        - pipeline1

--- demo_suraj2/pipelines/pipeline1/code/pipeline1/graph/__init__.py
+++ demo_suraj2/pipelines/pipeline1/code/pipeline1/graph/__init__.py
@@ -1,4 +1,4 @@
 from .ordering_the_dataset import ordering_the_dataset
 from .dataset2_0 import dataset2_0
 from .dataset1 import dataset1
-from .limit_600 import limit_600
+from .limit_700 import limit_700
